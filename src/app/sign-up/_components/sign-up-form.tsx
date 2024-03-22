2e09660c84f21eaf952e8c6404268cc950ab68dc
Adam Ridhwan <adamridhwan.1@gmail.com>
2024-03-21 20:02:24 -0400

    Add error handling and disable button when pending

--- src/app/sign-up/_components/sign-up-form.tsx
+++ src/app/sign-up/_components/sign-up-form.tsx
@@ -22,7 +22,7 @@ export const SignUpForm = () => {
 
   useEffect(() => {
     // TODO: Hacky way to do this. Need to fix this when React team releases a better way to handle this.
-    setError(state.message);
+    if (!state.success) setError(state.message);
   }, [state]);
 
   return (
@@ -48,11 +48,9 @@ export const SignUpForm = () => {
         className='mb-3 rounded-none border-x-0 border-b-2 border-t-0 border-b-darkBlue py-6 text-[13px] font-light focus-visible:ring-red'
         onFocus={() => setError('')}
       />
-
       <div className='h-10 pt-2 text-red'>
         {<AppFonts.bodyMedium className='font-medium'>{error}</AppFonts.bodyMedium>}
       </div>
-
       <SignUpButton />
     </form>
   );
