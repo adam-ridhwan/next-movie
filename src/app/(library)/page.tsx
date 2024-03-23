076fb546a791487aa94432f346fd1fcba6c734d5
Adam Ridhwan <adamridhwan.1@gmail.com>
2024-03-22 20:37:19 -0400

    Add ESLint rule for no-unused-vars

--- src/app/(library)/page.tsx
+++ src/app/(library)/page.tsx
@@ -1,12 +1,8 @@
-import { getServerSession } from 'next-auth';
-
 import { AppFonts } from '../_components/app-fonts';
 import { LibraryStrings } from '../_components/app-strings';
 import TrendingCarousel from './_components/trending-carousel';
 
 export default async function Home() {
-  const session = await getServerSession();
-
   return (
     <>
       <div className=''>
@@ -15,7 +11,7 @@ export default async function Home() {
           <TrendingCarousel />
         </div>
 
-        <div className='pl-10'>
+        <div className='pl-10 pt-4'>
           <AppFonts.headingMedium>{LibraryStrings.recommendedForYou}</AppFonts.headingMedium>
           <div>2-column vertical cards</div>
         </div>
