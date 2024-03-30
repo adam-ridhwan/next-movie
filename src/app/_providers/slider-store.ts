8e2d43790b5b5421ac2e55ea5f2361fb16630c86
Adam Ridhwan <adamridhwan.1@gmail.com>
2024-03-29 21:53:40 -0400

    clean

--- src/app/_providers/slider-store.ts
+++ src/app/_providers/slider-store.ts
@@ -1,14 +1,12 @@
 'use client';
 
-import chalk from 'chalk';
 import { create } from 'zustand';
+import { devtools } from 'zustand/middleware';
 
-import { DEVELOPMENT_MODE } from '@/app/_lib/utils';
 import { sliderUtils } from '@/app/(library)/_components/app-slider/slider-utils';
 import { Card } from '@/app/(library)/page';
 
-// eslint-disable-next-line no-console,@typescript-eslint/no-unused-vars
-const log = (x: string) => DEVELOPMENT_MODE && console.log(chalk.bgCyan.black(x));
+type Pages = Map<number, Card[]> | Array<[number, Card[]]>;
 
 type State = {
   CARDS: Card[];
@@ -32,10 +30,7 @@ type Actions = {
   goToNextPage: () => void;
   goToPrevPage: () => void;
   goToLastPage: () => void;
-  setPages: (
-    pages: Map<number, Card[]> | Array<[number, Card[]]>,
-    trailingCardsTotal: number
-  ) => void;
+  setPages: (pages: Pages, trailingCardsTotal: number) => void;
   resetPages: () => void;
   setCache: (pages: [number, Card[]][]) => void;
   getCache: () => Array<[number, Card[]]>;
@@ -50,170 +45,100 @@ type Actions = {
 
 export type SliderStore = State & Actions;
 
-export const createSliderStore = (CARDS: Card[]) => {
-  return create<SliderStore>((set, get) => ({
-    CARDS: CARDS,
+export const createSliderStore = (CARDS: Card[]) =>
+  create(
+    devtools<SliderStore>((set, get) => ({
+      CARDS: CARDS,
+      cache: '',
+      cardsPerPage: sliderUtils.getCardsPerPage(),
+      currentPage: 1,
+      hasPaginated: false,
+      isAnimating: false,
+      isFirstPageVisited: true,
+      isLastPageVisited: false,
+      maxPage: Math.ceil(CARDS.length / sliderUtils.getCardsPerPage()),
+      pages: new Map<number, Card[]>().set(1, CARDS.slice(0, 7)),
+      trailingCardsTotal: 0,
+      translatePercentage: 0,
 
-    pages: new Map<number, Card[]>().set(1, CARDS.slice(0, 7)),
-    maxPage: Math.ceil(CARDS.length / sliderUtils.getCardsPerPage()),
-    isFirstPageVisited: true,
-    isLastPageVisited: false,
-    setPages: (pages, trailingCardsTotal) => {
-      // log('setPages()');
-      set(() => {
-        return { pages: new Map(pages), cache: JSON.stringify(pages), trailingCardsTotal };
-      });
-    },
-    resetPages: () => {
-      // log('resetPages()');
-      set(() => {
-        return { pages: new Map() };
-      });
-    },
-
-    currentPage: 1,
-    setCurrentPage: currentPage => {
-      // log('setCurrentPage()');
-      set(() => {
-        return { currentPage };
-      });
-    },
-    goToNextPage: () => {
-      // log('goToNextPage()');
-      set(state => {
-        return { currentPage: state.currentPage + 1 };
-      });
-    },
-    goToPrevPage: () => {
-      // log('goToPrevPage()');
-      set(state => {
-        return { currentPage: state.currentPage - 1 };
-      });
-    },
-    goToLastPage: () => {
-      // log('goToLastPage()');
-      set(state => {
-        return { currentPage: state.maxPage };
-      });
-    },
-
-    resetToFirstPage: () => {
-      // log('resetToFirstPage()');
-      set(state => {
-        const cardsBeforeFirstIndex = state.CARDS.slice(-state.cardsPerPage);
-        const cardsAfterFirstIndex = state.getCache();
-        const newPages: [number, Card[]][] = [[0, cardsBeforeFirstIndex], ...cardsAfterFirstIndex];
-
-        return {
-          currentPage: 1,
-          pages: new Map(newPages),
-          isFirstPageVisited: true,
-          isLastPageVisited: false,
-        };
-      });
-    },
-    updateCardsWhenOnLastPage: () => {
-      // log('updateCardsWhenOnLastPage()');
-      set(state => {
-        const newCards: Card[] = [];
-        const cardsTotal = state.maxPage * state.cardsPerPage;
-
-        let decrementingCardIndex = CARDS.length - 1;
-        for (let i = cardsTotal; i > 0; i--) {
-          newCards.unshift(CARDS[decrementingCardIndex--]);
-          if (decrementingCardIndex === -1) {
-            decrementingCardIndex = CARDS.length - 1;
-          }
+      disableAnimation: () => set(() => ({ isAnimating: false })),
+      enableAnimation: () => set(() => ({ isAnimating: true })),
+      setCardsPerPage: cardsPerPage => set(() => ({ cardsPerPage })),
+      goToLastPage: () => set(state => ({ currentPage: state.maxPage })),
+      goToNextPage: () => set(state => ({ currentPage: state.currentPage + 1 })),
+      goToPrevPage: () => set(state => ({ currentPage: state.currentPage - 1 })),
+      resetPages: () => set(() => ({ pages: new Map() })),
+      markAsPaginated: () => set(() => ({ hasPaginated: true })),
+      setTrailingCardsTotal: trailingCardsTotal => set(() => ({ trailingCardsTotal })),
+      setTranslatePercentage: translatePercentage => set(() => ({ translatePercentage })),
+      setCurrentPage: currentPage => set(() => ({ currentPage })),
+      setIsAnimating: (isAnimating: boolean) => set(() => ({ isAnimating })),
+      setCache: pages => set(() => ({ cache: JSON.stringify(pages) })),
+      getCache: () => {
+        const state = get();
+        try {
+          return JSON.parse(state.cache);
+        } catch (error) {
+          console.error('Failed to parse cache:', error);
+          return new Map();
         }
+      },
+      resetToFirstPage: () => {
+        set(state => {
+          const cardsBeforeFirstIndex = state.CARDS.slice(-state.cardsPerPage);
+          const cardsAfterFirstIndex = state.getCache();
+          const newPages: [number, Card[]][] = [
+            [0, cardsBeforeFirstIndex],
+            ...cardsAfterFirstIndex,
+          ];
 
-        // Need to add the first few cards to the end,
-        // so that the cards are aligned properly
-        newCards.push(...CARDS.slice(0, state.cardsPerPage));
+          return {
+            currentPage: 1,
+            pages: new Map(newPages),
+            isFirstPageVisited: true,
+            isLastPageVisited: false,
+          };
+        });
+      },
+      setPages: (pages, trailingCardsTotal) =>
+        set(() => ({
+          pages: new Map(pages),
+          cache: JSON.stringify(pages),
+          trailingCardsTotal,
+        })),
+      updateCardsWhenOnLastPage: () => {
+        set(state => {
+          const newCards: Card[] = [];
+          const cardsTotal = state.maxPage * state.cardsPerPage;
 
-        const newPages: [number, Card[]][] = Array.from(
-          { length: state.maxPage + 1 },
-          (_, pageIndex) => {
-            const startIndex = pageIndex * state.cardsPerPage;
-            const endIndex = startIndex + state.cardsPerPage;
-            const newCardsGroup = newCards.slice(startIndex, endIndex);
-            return [pageIndex + 1, newCardsGroup];
+          let decrementingCardIndex = CARDS.length - 1;
+          for (let i = cardsTotal; i > 0; i--) {
+            newCards.unshift(CARDS[decrementingCardIndex--]);
+            if (decrementingCardIndex === -1) {
+              decrementingCardIndex = CARDS.length - 1;
+            }
           }
-        );
 
-        return {
-          pages: new Map<number, Card[]>(newPages),
-          isFirstPageVisited: false,
-          isLastPageVisited: true,
-        };
-      });
-    },
+          // Need to add the first few cards to the end,
+          // so that the cards are aligned properly
+          newCards.push(...CARDS.slice(0, state.cardsPerPage));
 
-    cache: '',
-    setCache: pages => {
-      // log('setCache()');
-      set(() => {
-        return { cache: JSON.stringify(pages) };
-      });
-    },
-    getCache: () => {
-      // log('getCache()');
-      const state = get();
-      try {
-        return JSON.parse(state.cache);
-      } catch (error) {
-        console.error('Failed to parse cache:', error);
-        return new Map();
-      }
-    },
+          const newPages: [number, Card[]][] = Array.from(
+            { length: state.maxPage + 1 },
+            (_, pageIndex) => {
+              const startIndex = pageIndex * state.cardsPerPage;
+              const endIndex = startIndex + state.cardsPerPage;
+              const newCardsGroup = newCards.slice(startIndex, endIndex);
+              return [pageIndex + 1, newCardsGroup];
+            }
+          );
 
-    cardsPerPage: sliderUtils.getCardsPerPage(),
-    setCardsPerPage: cardsPerPage => {
-      // log('setCardsPerPage()');
-      set(() => {
-        return { cardsPerPage };
-      });
-    },
-
-    trailingCardsTotal: 0,
-    setTrailingCardsTotal: trailingCardsTotal => {
-      // log('setTrailingCardsTotal()');
-      set(() => {
-        return { trailingCardsTotal };
-      });
-    },
-
-    translatePercentage: 0,
-    setTranslatePercentage: translatePercentage => {
-      // log('setTranslatePercentage()');
-      set(() => ({ translatePercentage }));
-    },
-
-    hasPaginated: false,
-    markAsPaginated: () => {
-      // log('markAsPaginated()');
-      set(() => {
-        return { hasPaginated: true };
-      });
-    },
-
-    isAnimating: false,
-    setIsAnimating: (isAnimating: boolean) => {
-      // log('setIsAnimating()');
-      set(() => {
-        return { isAnimating };
-      });
-    },
-    enableAnimation: () => {
-      // log('enableAnimation()');
-      set(() => {
-        return { isAnimating: true };
-      });
-    },
-    disableAnimation: () => {
-      // log('disableAnimation()');
-      set(() => {
-        return { isAnimating: false };
-      });
-    },
-  }));
-};
+          return {
+            pages: new Map<number, Card[]>(newPages),
+            isFirstPageVisited: false,
+            isLastPageVisited: true,
+          };
+        });
+      },
+    }))
+  );
