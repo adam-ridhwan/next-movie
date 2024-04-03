0420145b8665bb9807c70d6ab1bfeaa528a4e35e
Adam Ridhwan <adamridhwan.1@gmail.com>
2024-04-03 12:39:12 -0400

    instruction comments

--- src/components/slider/hooks/use-pagination/use-go-to-resized-page.ts
+++ src/components/slider/hooks/use-pagination/use-go-to-resized-page.ts
@@ -1,49 +1,81 @@
 /* eslint no-restricted-imports: 0 */
 
-import { Tile } from '@/lib/types';
+import { Pages, Tile } from '@/lib/types';
+import { usePages } from '@/components/slider/hooks/use-pages';
 import { log } from '@/components/slider/hooks/use-pagination/use-pagination';
 
 export const useGoToResizedPage = () => {
-  /* ────────────────────────────────────────────────────────────────────
-   * RESIZING FROM 4 TILES TO 3 TILES
-   *
-   * ────── FIRST PAGE ──────
-   * if the new current page is 1 => { goToFirstPage() }
-   *
-   *
-   * ────── SECOND PAGE ──────
-   * if the current page is > 1 => { goToResizedPage(previousTiles) }
-   *
-   * 1) get the previous current tiles
-   * 2) update the new current tiles with the previous current tiles
-   *
-   *     PAGE 0: [6, 7, 8, 9] => PAGE 0: [8, 9, 1]
-   *     PAGE 1: [1, 2, 3, 4] => PAGE 1: [2, 3, 4]
-   *  -> PAGE 2: [5, 6, 7, 8] => PAGE 2: [5, 6, 7] <-
-   *     PAGE 3: [9, 1, 2, 3] => PAGE 3: [8, 9, 1]
-   *     PAGE 4: [4, 5, 6, 7] => PAGE 4: [2, 3, 4]
+  const { getTilesPerPage, getMaxPages } = usePages();
+
+  /** ────────────────────────────────────────────────────────────────────
+   * From 4 tiles to 3 tiles per page (minimizing)
    *
    *
-   * ────── LAST PAGE ──────
-   * if the current page is the last page => { goToResizedPage(previousTiles) }
+   * 2nd page: -
+   * page 0: [6, 7, 8, 9] => page 0: [8, 9, 1]
+   * page 1: [1, 2, 3, 4] => page 1: [2, 3, 4]
+   * page 2: [5, 6, 7, 8] => page 2: [5, 6, 7] <- currentPage
+   * page 3: [9, 1, 2, 3] => page 3: [8, 9, 1]
+   * page 4: [4, 5, 6, 7] => page 4: [2, 3, 4]
    *
-   * 1) Get the previous current tiles
-   * 2) Update the new current tiles with the previous current tiles
    *
-   *     PAGE 0: [7, 8, 9, 1] => PAGE 0: [9, 1, 2]
-   *     PAGE 1: [2, 3, 4, 5] => PAGE 1: [3, 4, 5]
-   *  -> PAGE 2: [6, 7, 8, 9] => PAGE 2: [6, 7, 8] <-
-   *     PAGE 3: [9, 1, 2, 3] => PAGE 3: [9, 1, 2]
-   *     PAGE 4: [4, 5, 6, 7] => PAGE 4: [3, 4, 5]
+   * last page: -
+   * page 0: [3, 4, 5, 6] => page 0: [6, 7, 8]
+   * page 1: [7, 8, 9, 1] => page 1: [9, 1, 2]
+   * page 2: [2, 3, 4, 5] => page 2: [3, 4, 5]
+   * page 3: [6, 7, 8, 9] => page 3: [6, 7, 8] <- currentPage
+   * page 4: [1, 2, 3, 4] => page 4: [9, 1, 2]
+   *
+   *
+   * [1, 2, 3, 4, 5, 6, 7, 8, 9]
+   *
+   * ──────────────────────────────────────────────────────────────────
+   * From 3 tiles to 4 tiles per page (maximizing)
+   *
+   * 2nd page: -
+   * page 0: [7, 8, 9] => page 0: [5, 6, 7, 8]
+   * page 1: [1, 2, 3] => page 1: [9, 1, 2, 3]
+   * PAGE 2: [4, 5, 6] => PAGE 2: [4, 5, 6, 7] <- currentPage
+   * page 3: [7, 8, 9] => page 3: [8, 9, 1, 2]
+   * page 4: [1, 2, 3] => page 4: [3, 4, 5, 6]
+   *
+   * last page: -
+   * page 0: [7, 8, 9] => page 0: [4, 5, 6, 7]
+   * page 1: [1, 2, 3] => page 1: [8, 9, 1, 2]
+   * page 2: [4, 5, 6] => page 2: [3, 4, 5, 6]
+   * PAGE 3: [7, 8, 9] => PAGE 3: [7, 8, 9, 1] <- currentPage
+   * page 4: [1, 2, 3] => page 4: [2, 3, 4, 5]
+   *
+   * continue maximizing...
+   *
+   * From 4 tiles to 5 tiles per page (maximizing)
+   * page 0: [4, 5, 6, 7] => page 0: [1, 2, 3, 4, 5]
+   * page 1: [8, 9, 1, 2] => page 1: [6, 7, 8, 9, 1]
+   * page 2: [3, 4, 5, 6] => page 2: [2, 3, 4, 5, 6]
+   * PAGE 3: [7, 8, 9, 1] => PAGE 3: [7, 8, 9, 1, 2]
+   * page 4: [2, 3, 4, 5] => page 4: [3, 4, 5, 6, 7]
+   *
+   *
+   *
+   * [1, 2, 3, 4, 5, 6, 7, 8, 9]
    * ────────────────────────────────────────────────────────────────── */
 
-  const goToResizedPage = (previousTiles: Tile[]) => {
+  const goToResizedPage = (prevPage: Tile[]) => {
     log('RESIZED');
 
+    const firstItemOfPreviousPage = prevPage.at(0)?.id;
+
+    console.group('goToResizedPage');
     console.log(
-      'previousTiles',
-      previousTiles.map(tile => tile.id)
+      'prevPage:',
+      prevPage.map(tile => tile.id)
     );
+    console.log('firstItemOfPreviousPage:', firstItemOfPreviousPage);
+
+    const tilesPerPage = getTilesPerPage();
+    const maxPages = getMaxPages();
+
+    const newPages: Pages = new Map<number, Tile[]>();
   };
 
   return { goToResizedPage };
