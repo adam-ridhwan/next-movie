5123743d9043f73ae0dc17b9528c0ee277fc3364
Adam Ridhwan <adamridhwan.1@gmail.com>
2024-04-05 11:33:25 -0400

    add example comment

--- src/components/slider/hooks/use-pagination/use-minimized-page.ts
+++ src/components/slider/hooks/use-pagination/use-minimized-page.ts
@@ -10,9 +10,8 @@ import { useValidators } from '@/components/slider/hooks/use-validators';
 
 export const useMinimizedPage = () => {
   /** ────────────────────────────────────────────────────────────────────
-   * ✅ From 4 tiles to 3 tiles per page (minimizing)
+   * From [4] tiles to [3] tiles per page
    *
-   * 2nd page: -
    *     page 0: [6, 7, 8, 9] => page 0: [5, 6, 7]
    *     page 1: [1, 2, 3, 4] => page 1: [8, 9, 1]
    *  -> PAGE 2: [5, 6, 7, 8] => page 2: [2, 3, 4]
@@ -20,16 +19,16 @@ export const useMinimizedPage = () => {
    *     page 4: [4, 5, 6, 7] => page 4: [8, 9, 1]
    *                          => page 5: [2, 3, 4]
    *
-   * 2nd page: -
+   * continue minimizing...
+   *
    *     page 0: [5, 6, 7] => page 0: [8, 9]
    *     page 1: [8, 9, 1] => page 1: [1, 2]
    *     page 2: [2, 3, 4] => page 2: [3, 4]
-   *  -> PAGE 3: [5, 6, 7] => page 3: [5, 6] <-
-   *     page 4: [8, 9, 1] => PAGE 4: [7, 8]
+   *  -> PAGE 3: [5, 6, 7] => PAGE 3: [5, 6] <-
+   *     page 4: [8, 9, 1] => page 4: [7, 8]
    *     page 5: [2, 3, 4] => page 5: [9, 1]
    *                       => page 6: [2, 3]
-
-   * 3rd page: -
+   * ──────────────────────────────────────────────────────────────────
    *     page 0: [3, 4, 5, 6] => page 0: [6, 7, 8]
    *     page 1: [7, 8, 9, 1] => page 1: [9, 1, 2]
    *     page 2: [2, 3, 4, 5] => page 2: [3, 4, 5]
@@ -38,9 +37,7 @@ export const useMinimizedPage = () => {
    *                          => page 5: [3, 4, 5]
    *
    * continue minimizing...
-   * From 3 tiles to 2 tiles per page (minimizing)
    *
-   * 3rd page: -
    *     page 0: [6, 7, 8] => page 0: [7, 8]
    *     page 1: [9, 1, 2] => page 1: [9, 1]
    *     page 2: [3, 4, 5] => page 2: [2, 3]
