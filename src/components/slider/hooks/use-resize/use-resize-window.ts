b11812ffe1ba390fa81cfc460a735525483a241f
Adam Ridhwan <adamridhwan.1@gmail.com>
2024-04-11 12:37:02 -0400

    switch from resize listener to resize observer

--- src/components/slider/hooks/use-resize/use-resize-window.ts
+++ src/components/slider/hooks/use-resize/use-resize-window.ts
@@ -1,6 +1,6 @@
 /* eslint no-restricted-imports: 0 */
 
-import { useEffect, useRef } from 'react';
+import { useCallback, useEffect, useRef } from 'react';
 import chalk from 'chalk';
 
 import { RESIZE_DIRECTION } from '@/lib/constants';
@@ -14,8 +14,8 @@ const log = (label: string) => logger(chalk.bgHex('#FC86F3').black(`${label}`));
 
 export const useResizeWindow = () => {
   const {
-    state: { TILES, currentPage, pages },
-    actions: { goToFirstPage, goToLastPage, goToMinimizedPage, goToMaximizedPage },
+    state: { currentPage, pages },
+    actions: { goToFirstPage, goToMinimizedPage, goToMaximizedPage },
   } = usePagination();
   const { getTilesPerPage } = usePageUtils();
   const { resizeDirection } = useResizeDirection();
@@ -23,51 +23,43 @@ export const useResizeWindow = () => {
   const prevTilesPerPage = useRef(getTilesPerPage());
   const prevWindowWidth = useRef(typeof window === 'undefined' ? 0 : window.innerWidth);
 
-  useEffect(() => {
-    const handleResize = () => {
-      /*
-       * TODO: When using shortcut to adjust the window size, it breaks the slider.
-       *  The component getting the error is the right placeholder component.
-       *  1) To replicate: Minimize the browser until there is only 2 tiles per page.
-       *  2) Then, use keyboard shortcut to adjust the browser, there should be 4 tiles per page.
-       *  3) The right placeholder component will break.
-       */
-      const currentWidth = window.innerWidth;
-      const tilesPerPage = getTilesPerPage();
+  const handleResize = useCallback(() => {
+    const currentWidth = window.innerWidth;
+    const tilesPerPage = getTilesPerPage();
 
-      if (tilesPerPage === prevTilesPerPage.current) return;
-      prevTilesPerPage.current = tilesPerPage;
+    if (tilesPerPage === prevTilesPerPage.current) return;
+    prevTilesPerPage.current = tilesPerPage;
 
-      log(' USE WINDOW RESIZE ');
+    log(' USE WINDOW RESIZE ');
 
-      const prevPage = getMapItem({
-        label: 'handleResize() - prevPage',
-        map: pages,
-        key: currentPage,
-      });
+    const prevPage = getMapItem({
+      label: 'handleResize() - prevPage',
+      map: pages,
+      key: currentPage,
+    });
 
-      if (currentPage === 1) {
-        goToFirstPage();
-      } else {
-        resizeDirection === RESIZE_DIRECTION.MINIMIZING
-          ? goToMinimizedPage(prevPage)
-          : goToMaximizedPage(prevPage);
-      }
+    if (currentPage === 1) {
+      goToFirstPage();
+    } else {
+      resizeDirection === RESIZE_DIRECTION.MINIMIZING
+        ? goToMinimizedPage(prevPage)
+        : goToMaximizedPage(prevPage);
+    }
 
-      prevWindowWidth.current = currentWidth;
-    };
-
-    window.addEventListener('resize', handleResize);
-    return () => window.removeEventListener('resize', handleResize);
+    prevWindowWidth.current = currentWidth;
   }, [
-    TILES,
-    currentPage,
     pages,
+    currentPage,
     resizeDirection,
     getTilesPerPage,
     goToFirstPage,
-    goToLastPage,
-    goToMinimizedPage,
     goToMaximizedPage,
+    goToMinimizedPage,
   ]);
+
+  useEffect(() => {
+    const resizeObserver = new ResizeObserver(() => handleResize());
+    resizeObserver.observe(document.body);
+    return () => resizeObserver.disconnect();
+  }, [handleResize]);
 };
