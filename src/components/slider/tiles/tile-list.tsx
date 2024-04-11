4c58fcd241d13380f770b93448152f48d4137396
Adam Ridhwan <adamridhwan.1@gmail.com>
2024-04-11 15:59:39 -0400

    pages array

--- src/components/slider/tiles/tile-list.tsx
+++ src/components/slider/tiles/tile-list.tsx
@@ -1,18 +1,55 @@
+import { Fragment } from 'react';
+import { useDomContext } from '@/providers/dom-provider';
+
 import { DEVELOPMENT_MODE } from '@/lib/constants';
-import { cn } from '@/lib/utils';
+import { Tile } from '@/lib/types';
+import { cn, getMapItem } from '@/lib/utils';
 import { useAnimation } from '@/components/slider/hooks/use-animation';
 import { usePageUtils } from '@/components/slider/hooks/use-page-utils';
+import { usePagination } from '@/components/slider/hooks/use-pagination/use-pagination';
 import { useSlide } from '@/components/slider/hooks/use-slide';
-import CurrentPage from '@/components/slider/tiles/current-page';
-import LeftPlaceholder from '@/components/slider/tiles/left-placeholder';
-import NextPage from '@/components/slider/tiles/next-page';
-import PrevPage from '@/components/slider/tiles/prev-page';
-import RightPlaceholder from '@/components/slider/tiles/right-placeholder';
+import TileItem from '@/components/slider/tiles/tile-item';
 
 const TileList = () => {
+  const {
+    state: { pages, currentPage },
+  } = usePagination();
   const { hasPaginated } = usePageUtils();
   const { slideAmount } = useSlide();
   const { isAnimating } = useAnimation();
+  const { isMounted } = usePageUtils();
+  const { tileRef } = useDomContext();
+
+  // TODO: Get the left and right placeholder tiles
+  //  [ ] LeftPlaceholder
+  //  [ ] RightPlaceholder
+  //  [ ] Refactor the code and possibly extract the logic to a separate hook
+  //  [ ] Delete old code
+
+  const prevPageTiles =
+    !isMounted || !hasPaginated
+      ? []
+      : getMapItem({
+          label: 'LeftPlaceholder: prevPage',
+          map: pages,
+          key: currentPage - 1,
+        });
+
+  const currentPageTiles = getMapItem({
+    label: 'CurrentPage: currentPageTiles',
+    map: pages,
+    key: currentPage,
+  });
+
+  const nextPageTiles = !isMounted
+    ? []
+    : getMapItem({
+        label: 'NextPage: nextPageTiles',
+        map: pages,
+        key: currentPage + 1,
+      });
+
+  const tilesToRender: Tile[] = [...prevPageTiles, ...currentPageTiles, ...nextPageTiles];
 
   return (
     <>
@@ -27,11 +64,24 @@ const TileList = () => {
           transform: slideAmount ? `translate3d(${slideAmount}%, 0, 0)` : undefined,
         }}
       >
-        <LeftPlaceholder />
-        <PrevPage />
-        <CurrentPage />
-        <NextPage />
-        <RightPlaceholder />
+        {tilesToRender.map((tile, i) => (
+          <Fragment key={i}>
+            <TileItem
+              ref={i === 0 ? tileRef : undefined}
+              tile={tile}
+              displayNumber={i + 1}
+              isVisibleOnScreen={true}
+            />
+          </Fragment>
+        ))}
+        {/*{pagesArray.map((page, i) => (*/}
+        {/*  <TileItem key={`TileItem-${i}`} tile={page} />*/}
+        {/*))}*/}
+        {/*<LeftPlaceholder />*/}
+        {/*<PrevPage />*/}
+        {/*<CurrentPage />*/}
+        {/*<NextPage />*/}
+        {/*<RightPlaceholder />*/}
       </div>
     </>
   );
