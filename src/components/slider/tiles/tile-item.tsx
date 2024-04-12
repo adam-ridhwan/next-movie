fe5519ffc8a84a251cd4181635ac88878b9faf72
Adam Ridhwan <adamridhwan.1@gmail.com>
2024-04-12 15:42:12 -0400

    update styling for development mode

--- src/components/slider/tiles/tile-item.tsx
+++ src/components/slider/tiles/tile-item.tsx
@@ -19,25 +19,33 @@ const TileItem: ForwardRefRenderFunction<HTMLDivElement, TileItemProps> = (
   return (
     <div ref={ref} className={cn('slider-tile p-1', `tile-${isVisibleOnScreen ? displayNumber : ''}`)}>
       {DEVELOPMENT_MODE && (
-        // <div
-        //   className='relative flex aspect-video flex-col items-center justify-center gap-1
-        //   text-8xl outline outline-black'
-        // >
-        //   {tile.id}
-        //   <div className='absolute right-1 top-0 text-4xl'>{tile.id}</div>
-        //   <div className='absolute left-1 top-0 text-4xl'>{tile.id}</div>
-        // </div>
-
-        <div className='relative flex aspect-video flex-col justify-end gap-1 p-4'>
-          <Image
-            src={tile.thumbnailUrl}
-            alt='thumbnail'
-            priority
-            fill
-            sizes='(min-width: 1536px) 16.66vw, (min-width: 1280px) 20vw, (min-width: 1024px) 25vw, (min-width: 768px) 33.33vw, 50vw'
-            className='object-cover'
-          />
-        </div>
+        <>
+          <div className='relative flex aspect-video flex-col justify-end gap-1 p-4'>
+            <Image
+              src={tile.thumbnailUrl}
+              alt='thumbnail'
+              priority
+              fill
+              sizes='(min-width: 1536px) 16.66vw, (min-width: 1280px) 20vw, (min-width: 1024px) 25vw, (min-width: 768px) 33.33vw, 50vw'
+              className='object-cover opacity-25'
+            />
+            <div
+              style={{
+                position: 'absolute',
+                top: 0,
+                left: 0,
+                right: 0,
+                bottom: 0,
+                backgroundColor: 'rgba(0, 0, 0, 0.4)',
+              }}
+            />
+            <div className='absolute right-1/2 top-1/2 -translate-y-1/2 translate-x-1/2 gap-1 text-8xl'>
+              {tile.title}
+            </div>
+            <div className='absolute right-1 top-0 text-4xl'>{tile.title}</div>
+            <div className='absolute left-1 top-0 text-4xl'>{tile.title}</div>
+          </div>
+        </>
       )}
 
       {!DEVELOPMENT_MODE && (
