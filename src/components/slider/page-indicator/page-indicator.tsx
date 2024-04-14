554dde000247e95348f96236dd211886374e4041
Adam Ridhwan <adamridhwan.1@gmail.com>
2024-04-14 12:30:00 -0400

    adjust padding for page indicator

--- src/components/slider/page-indicator/page-indicator.tsx
+++ src/components/slider/page-indicator/page-indicator.tsx
@@ -7,7 +7,7 @@ const PageIndicator = () => {
   } = usePagination();
 
   return (
-    <div className='absolute -top-5 right-0 flex items-center justify-center px-12 opacity-0 group-hover/slider:opacity-100'>
+    <div className='pr-leftRightCustom absolute -top-5 right-0 flex items-center justify-center opacity-100 group-hover/slider:opacity-100'>
       {Array.from({ length: pages.size - 2 }).map((_, index) => (
         <PageIndicatorIcon key={index} isActive={currentPage - 1 === index} />
       ))}
