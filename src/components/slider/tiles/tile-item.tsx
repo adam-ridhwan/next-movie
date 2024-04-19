89dc59dc4616d104a6c8b009fd850e7345949c9a
Adam Ridhwan <adamridhwan.1@gmail.com>
2024-04-19 17:01:08 -0400

    images

--- src/components/slider/tiles/tile-item.tsx
+++ src/components/slider/tiles/tile-item.tsx
@@ -28,31 +28,11 @@ const TileItem: ForwardRefRenderFunction<HTMLDivElement, TileItemProps> = (
       ref={ref}
       className={cn('slider-tile', `tile-${isVisibleOnScreen && isMounted ? displayNumber : ''}`)}
     >
-      {!DEVELOPMENT_MODE && (
-        <div className='relative flex aspect-video flex-col justify-end overflow-hidden rounded-2xl shadow-tileShadow'>
-          <div
-            style={{
-              position: 'absolute',
-              top: 0,
-              left: 0,
-              right: 0,
-              bottom: 0,
-              backgroundColor: 'rgba(0, 0, 0, 0.5)',
-            }}
-          />
-          <div className='absolute right-1/2 top-1/2 -translate-y-1/2 translate-x-1/2 gap-1 text-8xl'>
-            {index}
-          </div>
-          <div className='absolute right-1 top-0 text-4xl'>{index}</div>
-          <div className='absolute left-1 top-0 text-4xl'>{index}</div>
-        </div>
-      )}
-
       <div className='relative flex aspect-video flex-col justify-end overflow-hidden rounded-2xl shadow-tileShadow'>
         {/* Image docs: https://developer.themoviedb.org/docs/image-basics */}
         {/* Example: https://image.tmdb.org/t/p/w500/1E5baAaEse26fej7uHcjOgEE2t2.jpg */}
         <Image
-          src={`https://image.tmdb.org/t/p/original${tile.backdrop_path ?? tile.poster_path}`}
+          src={`https://image.tmdb.org/t/p/w500${tile.backdrop_path ?? tile.poster_path}`}
           alt={tile.title || tile.name}
           priority
           fill
