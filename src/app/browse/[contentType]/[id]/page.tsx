9ad8f475fd852818a99d5064f132dc5b017e43d9
Adam Ridhwan <adamridhwan.1@gmail.com>
2024-04-21 16:48:48 -0400

    overlay

--- src/app/browse/[contentType]/[id]/page.tsx
+++ src/app/browse/[contentType]/[id]/page.tsx
@@ -1,3 +1,4 @@
+import * as React from 'react';
 import { Suspense } from 'react';
 import Backdrop from '@/browse/components/backdrop';
 import { Label } from '@/browse/components/label';
@@ -9,26 +10,35 @@ import { ContentRouteParams } from '@/lib/types';
 
 export default function ContentModal({ params: { contentType, id } }: { params: ContentRouteParams }) {
   return (
-    <Modal>
-      <Suspense fallback={<BackdropSkeleton />}>
-        <Backdrop {...{ contentType, id }} />
-      </Suspense>
+    <>
+      {/* Need this so that when we navigate directly to url, the overlay appears immediately */}
+      <div
+        className='fixed inset-0 z-50 bg-black/80
+        data-[state=open]:animate-in data-[state=closed]:animate-out
+        data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0'
+      />
 
-      <div className='flex flex-col gap-12 px-14 lg:flex-row'>
-        <div className='flex w-full flex-col gap-4 lg:w-3/5'>
-          <Suspense fallback={<OverviewSkeleton />}>
-            <Label {...{ contentType, id }} />
-          </Suspense>
-        </div>
+      <Modal>
+        <Suspense fallback={<BackdropSkeleton />}>
+          <Backdrop {...{ contentType, id }} />
+        </Suspense>
 
-        <div className='flex w-full flex-col gap-4 lg:w-2/5'>
-          <Suspense fallback={<MetadataSkeleton />}>
-            <Actors {...{ contentType, id }} />
-            <Genres {...{ contentType, id }} />
-            <Keywords {...{ contentType, id }} />
-          </Suspense>
+        <div className='flex flex-col gap-12 px-14 lg:flex-row'>
+          <div className='flex w-full flex-col gap-4 lg:w-3/5'>
+            <Suspense fallback={<OverviewSkeleton />}>
+              <Label {...{ contentType, id }} />
+            </Suspense>
+          </div>
+
+          <div className='flex w-full flex-col gap-4 lg:w-2/5'>
+            <Suspense fallback={<MetadataSkeleton />}>
+              <Actors {...{ contentType, id }} />
+              <Genres {...{ contentType, id }} />
+              <Keywords {...{ contentType, id }} />
+            </Suspense>
+          </div>
         </div>
-      </div>
-    </Modal>
+      </Modal>
+    </>
   );
 }
--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -40,7 +40,7 @@ const DialogContent = React.forwardRef<
   React.ComponentPropsWithoutRef<typeof DialogPrimitive.Content>
 >(({ className, children, ...props }, ref) => (
   <DialogPortal>
-    <DialogOverlay />
+    {/*<DialogOverlay />*/}
     <DialogPrimitive.Content
       ref={ref}
       className={cn(
