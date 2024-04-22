fdc92ba9a834246f9ac3eae96dc65ade4036d3bb
Adam Ridhwan <adamridhwan.1@gmail.com>
2024-04-21 21:24:46 -0400

    handles undefined

--- src/components/slider/tiles/tile-item.tsx
+++ src/components/slider/tiles/tile-item.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 
 import { Movie } from '@/lib/types';
 import { cn } from '@/lib/utils';
-import { BodyMedium, BodySmall } from '@/components/fonts';
+import { BodyMedium, BodySmall, HeadingExtraSmall } from '@/components/fonts';
 import { usePageUtils } from '@/components/slider/hooks/use-page-utils';
 import { usePagination } from '@/components/slider/hooks/use-pagination';
 
@@ -44,7 +44,9 @@ const TileItem: ForwardRefRenderFunction<HTMLDivElement, TileItemProps> = (
 
           <div className='pt-3'>
             <div className='flex flex-col'>
-              <BodyMedium className='line-clamp-1'>{tile.name || tile.original_title}</BodyMedium>
+              <BodyMedium className='line-clamp-1'>
+                {tile.name || tile.title || tile.original_title}
+              </BodyMedium>
               <BodySmall>{extractYear(tile.release_date || tile.first_air_date)}</BodySmall>
             </div>
           </div>
@@ -56,25 +58,34 @@ const TileItem: ForwardRefRenderFunction<HTMLDivElement, TileItemProps> = (
 
 const ContentImage = ({ tile }: { tile: Movie }) => {
   return (
-    <div className='relative flex aspect-video flex-col justify-end overflow-hidden rounded-2xl shadow-tileShadow max-sm:aspect-poster'>
+    <div className='relative flex aspect-video flex-col justify-end overflow-hidden rounded-2xl bg-muted/50 shadow-tileShadow max-sm:aspect-poster'>
       {/* Image docs: https://developer.themoviedb.org/docs/image-basics */}
       {/* Example: https://image.tmdb.org/t/p/w500/1E5baAaEse26fej7uHcjOgEE2t2.jpg */}
-      <Image
-        src={`https://image.tmdb.org/t/p/original${tile.backdrop_path || tile.poster_path}`}
-        alt={tile.title || tile.name}
-        priority
-        fill
-        sizes='(min-width: 1300px) 20vw, (min-width: 1000px) 25vw, (min-width: 800px) 33.33vw, 50vw'
-        className='object-cover max-sm:hidden'
-      />
-      <Image
-        src={`https://image.tmdb.org/t/p/original${tile.poster_path || tile.backdrop_path}`}
-        alt={tile.title || tile.name}
-        priority
-        fill
-        sizes='(min-width: 1300px) 20vw, (min-width: 1000px) 25vw, (min-width: 800px) 33.33vw, 50vw'
-        className='object-cover sm:hidden'
-      />
+
+      {tile.backdrop_path || tile.poster_path ? (
+        <>
+          <Image
+            src={`https://image.tmdb.org/t/p/original${tile.backdrop_path || tile.poster_path}`}
+            alt={tile.title || tile.name}
+            priority
+            fill
+            sizes='(min-width: 1300px) 20vw, (min-width: 1000px) 25vw, (min-width: 800px) 33.33vw, 50vw'
+            className='object-cover max-sm:hidden'
+          />
+          <Image
+            src={`https://image.tmdb.org/t/p/original${tile.poster_path || tile.backdrop_path}`}
+            alt={tile.title || tile.name}
+            priority
+            fill
+            sizes='(min-width: 1300px) 20vw, (min-width: 1000px) 25vw, (min-width: 800px) 33.33vw, 50vw'
+            className='object-cover sm:hidden'
+          />
+        </>
+      ) : (
+        <div className='absolute bottom-0 z-50 flex h-full w-full items-end justify-center bg-gradient-to-t from-black/50 via-transparent to-transparent py-8'>
+          <HeadingExtraSmall>{tile.name || tile.original_title || tile.original_name}</HeadingExtraSmall>
+        </div>
+      )}
     </div>
   );
 };
