8b09c71cb3d6cccbddd1c2be30e99a662ead211d
Adam Ridhwan <adamridhwan.1@gmail.com>
2024-04-26 21:45:08 -0400

    delete ts-ignore

--- src/routes/hooks.ts
+++ src/routes/hooks.ts
@@ -1,54 +1,41 @@
-import { useRouter } from "next/navigation";
 import {
   useParams as useNextParams,
-  useSearchParams as useNextSearchParams
-} from "next/navigation";
-import { z } from "zod";
+  useSearchParams as useNextSearchParams,
+  useRouter,
+} from 'next/navigation';
+import { z } from 'zod';
 
-import { RouteBuilder } from "./makeRoute";
+import { RouteBuilder } from './makeRoute';
 
 const emptySchema = z.object({});
 
-type PushOptions = Parameters<ReturnType<typeof useRouter>["push"]>[1];
+type PushOptions = Parameters<ReturnType<typeof useRouter>['push']>[1];
 
-export function usePush<
-  Params extends z.ZodSchema,
-  Search extends z.ZodSchema = typeof emptySchema
->(builder: RouteBuilder<Params, Search>) {
+export function usePush<Params extends z.ZodSchema, Search extends z.ZodSchema = typeof emptySchema>(
+  builder: RouteBuilder<Params, Search>
+) {
   const { push } = useRouter();
-  return (
-    p: z.input<Params>,
-    search?: z.input<Search>,
-    options?: PushOptions
-  ) => {
+  return (p: z.input<Params>, search?: z.input<Search>, options?: PushOptions) => {
     push(builder(p, search), options);
   };
 }
 
-export function useParams<
-  Params extends z.ZodSchema,
-  Search extends z.ZodSchema = typeof emptySchema
->(builder: RouteBuilder<Params, Search>): z.output<Params> {
+export function useParams<Params extends z.ZodSchema, Search extends z.ZodSchema = typeof emptySchema>(
+  builder: RouteBuilder<Params, Search>
+): z.output<Params> {
   const res = builder.paramsSchema.safeParse(useNextParams());
   if (!res.success) {
-    throw new Error(
-      `Invalid route params for route ${builder.routeName}: ${res.error.message}`
-    );
+    throw new Error(`Invalid route params for route ${builder.routeName}: ${res.error.message}`);
   }
   return res.data;
 }
 
-export function useSearchParams<
-  Params extends z.ZodSchema,
-  Search extends z.ZodSchema = typeof emptySchema
->(builder: RouteBuilder<Params, Search>): z.output<Search> {
-  const res = builder.searchSchema!.safeParse(
-    convertURLSearchParamsToObject(useNextSearchParams())
-  );
+export function useSearchParams<Params extends z.ZodSchema, Search extends z.ZodSchema = typeof emptySchema>(
+  builder: RouteBuilder<Params, Search>
+): z.output<Search> {
+  const res = builder.searchSchema!.safeParse(convertURLSearchParamsToObject(useNextSearchParams()));
   if (!res.success) {
-    throw new Error(
-      `Invalid search params for route ${builder.routeName}: ${res.error.message}`
-    );
+    throw new Error(`Invalid search params for route ${builder.routeName}: ${res.error.message}`);
   }
   return res.data;
 }
@@ -61,7 +48,7 @@ function convertURLSearchParamsToObject(
   }
 
   const obj: Record<string, string | string[]> = {};
-  // @ts-ignore
+
   for (const [key, value] of params.entries()) {
     if (params.getAll(key).length > 1) {
       obj[key] = params.getAll(key);
