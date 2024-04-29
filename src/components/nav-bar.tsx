a59d8c6d20d6929436be237323b8839872f512ec
Adam Ridhwan <adamridhwan.1@gmail.com>
2024-04-29 14:31:56 -0400

    fix navbar styling

--- src/components/nav-bar.tsx
+++ src/components/nav-bar.tsx
@@ -12,23 +12,25 @@ const NavBar = () => {
   const pathname = usePathname();
 
   return (
-    <div className='flex h-16 flex-row items-center justify-between bg-black px-leftRightCustom'>
-      <div className='relative flex w-full flex-row items-center gap-8'>
-        <LogoIcon />
-        <nav>
-          <BrowseRoute.Link>
-            <BodySmall
-              className={cn('transition-colors hover:text-primary/50', {
-                'text-primary': pathname === BrowseRoute(),
-                'text-primary/70': pathname !== BrowseRoute(),
-              })}
-            >
-              Browse
-            </BodySmall>
-          </BrowseRoute.Link>
-        </nav>
+    <div className='flex h-16 items-center bg-black'>
+      <div className='container flex flex-row items-center justify-between px-leftRightCustom'>
+        <div className='relative flex w-full flex-row items-center gap-8'>
+          <LogoIcon />
+          <nav>
+            <BrowseRoute.Link>
+              <BodySmall
+                className={cn('transition-colors hover:text-primary/50', {
+                  'text-primary': pathname === BrowseRoute(),
+                  'text-primary/70': pathname !== BrowseRoute(),
+                })}
+              >
+                Browse
+              </BodySmall>
+            </BrowseRoute.Link>
+          </nav>
 
-        <Search />
+          <Search />
+        </div>
       </div>
     </div>
   );
