39a878b03a12c616432e95ded7db28d39968e01c
Adam Ridhwan <adamridhwan.1@gmail.com>
2024-04-29 17:57:24 -0400

    delete semicolon

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -5,7 +5,7 @@ import SearchResult from '@/components/search-input/search-result';
 export default async function SearchPage() {
   return (
     <Suspense>
-      <SearchResult />;
+      <SearchResult />
     </Suspense>
   );
 }
