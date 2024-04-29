137a2c570abb3562d16a8053a1679c415266206a
Adam Ridhwan <adamridhwan.1@gmail.com>
2024-04-29 07:45:20 -0400

    add content type

--- src/app/browse/layout.tsx
+++ src/app/browse/layout.tsx
@@ -36,7 +36,7 @@ export default async function BrowseLayout({ children }: { children: ReactNode }
           section={content.section}
         >
           <DomContextProvider>
-            <Slider headerTitle={content.label} />
+            <Slider headerTitle={content.label || ''} />
           </DomContextProvider>
         </SliderProvider>
       ))}
