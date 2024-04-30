f6668d4e990c8246d163fdd356a8eb02dd9d2e71
Adam Ridhwan <adamridhwan.1@gmail.com>
2024-04-30 17:04:44 -0400

    movies page

--- src/app/movies/layout.tsx
+++ src/app/movies/layout.tsx
@@ -2,26 +2,47 @@ import { ReactNode } from 'react';
 import { fetchTMDB } from '@/actions/fetch-tmdb';
 import { SliderProvider } from '@/providers/slider/slider-provider';
 
-import { MovieList, MovieListSchema } from '@/types/tmdb';
+import { FetchTMDBParams } from '@/types/global';
+import { MovieListSchema } from '@/types/tmdb';
 import { isNullish } from '@/lib/utils';
 import EpicStage from '@/components/epic-stage/epic-stage';
 import Slider from '@/components/slider/slider';
 
 const MoviesLayout = async ({ children }: { children: ReactNode }) => {
+  const fetchTMDBParams: Array<FetchTMDBParams & { label: string }> = [
+    { label: 'Action Movies', category: 'discover', mediaType: 'movie', genreId: 28, page: 3 },
+    { label: 'Sci-Fi Movies', category: 'discover', mediaType: 'movie', genreId: 878, page: 3 },
+    { label: 'Trending: Movies', category: 'trending', mediaType: 'movie' },
+    { label: 'Popular: Movies', category: 'popular', mediaType: 'movie' },
+  ];
+
+  const content = await Promise.all(
+    fetchTMDBParams.map(async params => {
+      const media = await fetchTMDB({ ...params });
+      const { success, data, error } = MovieListSchema.safeParse(media);
+      if (!success) throw new Error(`BrowseLayout() Invalid Movies schema : ${error.message}`);
+
+      return {
+        ...params,
+        results: data.results,
+      };
+    })
+  );
   return (
     <>
       <EpicStage mediaType='movie' />
 
-      {/*{content.map(content => (*/}
-      {/*  <SliderProvider*/}
-      {/*    key={content.label}*/}
-      {/*    content={content.results}*/}
-      {/*    mediaType={content.mediaType}*/}
-      {/*    section={content.section}*/}
-      {/*  >*/}
-      {/*    <Slider headerTitle={isNullish(content.label)} />*/}
-      {/*  </SliderProvider>*/}
-      {/*))}*/}
+      {content.map(content =>
+        // prettier-ignore
+        <SliderProvider
+          key={content.label}
+          content={content.results}
+          mediaType='movie'
+          section='movie'
+        >
+          <Slider headerTitle={isNullish(content.label)} />
+        </SliderProvider>
+      )}
 
       {children}
     </>
