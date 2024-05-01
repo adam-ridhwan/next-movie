8e8ab48f5b295160370551a256c9e1592f5d260a
Adam Ridhwan <adamridhwan.1@gmail.com>
2024-04-30 22:50:48 -0400

    more like this

--- src/components/media-modal/more-like-this.tsx
+++ src/components/media-modal/more-like-this.tsx
@@ -2,21 +2,30 @@ import { fetchTMDB } from '@/actions/fetch-tmdb';
 import { SliderProvider } from '@/providers/slider/slider-provider';
 
 import { ContentRouteParams, FetchTMDBParams } from '@/types/global';
+import { MovieListSchema, TvListSchema } from '@/types/tmdb';
 import Slider from '@/components/slider/slider';
 
 export default async function MoreLikeThis({ id, mediaType }: ContentRouteParams) {
   const content: FetchTMDBParams[] = [
-    { category: 'recommendations', mediaType, id },
-    { category: 'similar', mediaType, id },
+    { mediaType, id, category: 'recommendations' },
+    { mediaType, id, category: 'similar' },
   ];
+  const schema = mediaType === 'movie' ? MovieListSchema : TvListSchema;
 
   const contentPromises = content.map(async content => {
     const movieTvs = await fetchTMDB({ ...content });
-    return { ...content, results: movieTvs.results ?? [] };
+
+    const { success, data, error } = schema.safeParse(movieTvs);
+    if (!success) throw new Error(`MoreLikeThis() Invalid ${mediaType} schema: ${error.message}`);
+
+    return {
+      ...content,
+      results: data.results,
+    };
   });
 
   const [recommendations, similar] = await Promise.all(contentPromises);
-  const moreLikesThis = recommendations.results.length > 0 ? recommendations.results : similar.results;
+  const moreLikesThis = recommendations.results.length ? recommendations.results : similar.results;
 
   if (!moreLikesThis.length) return null;
 
