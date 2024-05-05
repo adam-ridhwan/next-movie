cf6bcee07d217afad700147fed8eacc5fa34da4d
Adam Ridhwan <adamridhwan.1@gmail.com>
2024-05-04 20:45:03 -0400

    separate modals

--- src/app/@modal/[...slug]/page.tsx
+++ src/app/@modal/[...slug]/page.tsx
@@ -12,6 +12,7 @@ import {
   TV_GENRES,
 } from '@/types/global-types';
 import { extractGenreMediaTypeSlugs, getGenreIdBySlug } from '@/lib/utils';
+import GenreModal from '@/components/media-modal/genre/genre-modal';
 import MediaModal from '@/components/media-modal/media-modal';
 import Backdrop from '@/components/media-modal/movie-tv/backdrop';
 import BonusContent from '@/components/media-modal/movie-tv/bonus-content';
@@ -23,6 +24,7 @@ import {
   Keywords,
 } from '@/components/media-modal/movie-tv/metadata';
 import MoreLikeThis from '@/components/media-modal/movie-tv/more-like-this';
+import MovieTvModal from '@/components/media-modal/movie-tv/movie-tv-modal';
 import Overlay from '@/components/media-modal/movie-tv/overlay';
 import Trailers from '@/components/media-modal/movie-tv/trailers';
 import {
@@ -82,71 +84,3 @@ const MediaModalPage = async ({ params }: MediaPageProps) => {
   }
 };
 export default MediaModalPage;
-
-type MediaModalProps = {
-  mediaType: MediaType;
-  mediaId: string;
-};
-
-const MovieTvModal = ({ mediaType, mediaId }: MediaModalProps) => {
-  return (
-    <>
-      <Overlay />
-      <MediaModal>
-        <Suspense fallback={<BackdropSkeleton />}>
-          <Backdrop mediaType={mediaType} id={mediaId} />
-        </Suspense>
-
-        <div className='flex flex-col gap-12 px-custom py-4 pb-10 lg:flex-row'>
-          <div className='mx-[0.5%] flex w-full flex-col gap-4 lg:w-3/5'>
-            <Suspense fallback={<OverviewSkeleton />}>
-              <Label mediaType={mediaType} id={mediaId} />
-            </Suspense>
-          </div>
-
-          <div className='flex w-full flex-col justify-center gap-4 lg:w-2/5'>
-            <Suspense fallback={<MetadataSkeleton />}>
-              <Actors mediaType={mediaType} id={mediaId} />
-              <Genres mediaType={mediaType} id={mediaId} />
-              <Keywords mediaType={mediaType} id={mediaId} />
-            </Suspense>
-          </div>
-        </div>
-
-        <Suspense fallback={<TileLoadingSkeleton count={1} />}>
-          <MoreLikeThis mediaType={mediaType} id={mediaId} />
-        </Suspense>
-
-        <Suspense>
-          <Trailers mediaType={mediaType} id={mediaId} />
-        </Suspense>
-
-        <Suspense>
-          <BonusContent mediaType={mediaType} id={mediaId} />
-        </Suspense>
-
-        <Suspense fallback={<HeadshotsSkeleton />}>
-          <Cast mediaType={mediaType} id={mediaId} />
-        </Suspense>
-      </MediaModal>
-    </>
-  );
-};
-
-type GenreModalProps = {
-  slug: GenreSlug;
-  genreId: GenreId;
-};
-
-const GenreModal = ({ slug, genreId }: GenreModalProps) => {
-  return (
-    <>
-      <Overlay />
-      <MediaModal>
-        <div>
-          {slug}:{genreId}
-        </div>
-      </MediaModal>
-    </>
-  );
-};
