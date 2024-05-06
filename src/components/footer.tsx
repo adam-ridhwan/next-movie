1ad552c4be86923b44ef34f731fe95bfd5a8a012
Adam Ridhwan <adamridhwan.1@gmail.com>
2024-05-06 13:35:42 -0400

    footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,34 +1,54 @@
 import Image from 'next/image';
+import { Github } from 'lucide-react';
 
 import { BodySmall } from '@/components/fonts';
-import { NextjsIcon, VercelIcon } from '@/components/icons';
+import { GithubIcon, NextjsIcon, VercelIcon } from '@/components/icons';
 
-const Footer = () => {
-  return (
-    <footer className='container px-custom py-10'>
-      <div className='flex flex-col gap-2'>
-        <div className='flex h-10 items-center gap-2'>
-          <BodySmall className='text-sm'>Made with</BodySmall>
+const Footer = () => (
+  <footer className='container px-custom py-10'>
+    <div className='flex flex-col gap-2'>
+      <div className='flex h-10 items-center gap-2'>
+        <BodySmall className='text-sm'>Made with</BodySmall>
+        <a href='https://nextjs.org/' target='_blank' rel='noreferrer'>
           <NextjsIcon />
-        </div>
+        </a>
+      </div>
 
-        <div className='flex h-10 items-center gap-2'>
-          <BodySmall className='text-sm'>Data provided by</BodySmall>
+      <div className='flex h-10 items-center gap-2'>
+        <BodySmall className='text-sm'>Data provided by</BodySmall>
+        <a
+          href='https://developer.themoviedb.org/docs/getting-started'
+          target='_blank'
+          rel='noreferrer'
+        >
           <Image
             src='/tmdb-logo.svg'
             alt='The Movie Database Logo'
             width={100}
             height={0}
           />
-        </div>
-
-        <div className='flex h-10 items-center gap-2'>
-          <BodySmall className='text-sm'>Powered by</BodySmall>
-          <VercelIcon />
-        </div>
+        </a>
       </div>
-    </footer>
-  );
-};
+
+      <div className='flex h-10 items-center gap-2'>
+        <BodySmall className='text-sm'>Powered by</BodySmall>
+        <a href='https://vercel.com/' target='_blank' rel='noreferrer'>
+          <VercelIcon />
+        </a>
+      </div>
+
+      <div className='flex h-10 items-center gap-2'>
+        <BodySmall className='text-sm'>Contribute</BodySmall>
+        <a
+          href='https://github.com/adam-ridhwan/next-imdb'
+          target='_blank'
+          rel='noreferrer'
+        >
+          <GithubIcon />
+        </a>
+      </div>
+    </div>
+  </footer>
+);
 
 export default Footer;
